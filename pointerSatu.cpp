f3e1704d7bfdabae6ef295f0e8e3ab9a488c6d62
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-05-06 15:43:11 +0800

    pointer derefrence pMhs

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -16,5 +16,8 @@ int main (){
     refMhs.nim = 2; //member Access Operator
     mhs.showNim();
 
-
+    mahasiswa *pMhs = &mhs; //pointer Dereference pMhs
+    pMhs->nim = 3; //Arrow Operator
+    pMhs->showNim();
+    return 0;
 }
