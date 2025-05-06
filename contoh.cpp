0b67e6c2c3595021993f45dc53b7dfccffb6f2ec
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-05-06 15:11:21 +0800

    Membuat Class Lingkaran

--- contoh.cpp
+++ contoh.cpp
@@ -24,3 +24,20 @@ public: // Akses modifier
     Lingkaran ol;
 
 };
+
+class Lingkaran
+{
+public:
+    int jarijari;
+
+    void input()
+    {
+       cout << "Masukkan jari-jari : ";
+       cin >> jarijari; 
+    }
+
+    int luasLingkaran(int r)
+    {
+        return 3.14 * r * r;
+    }
+};
